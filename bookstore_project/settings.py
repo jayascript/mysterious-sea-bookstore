571f4e62e7087e5e61745b0f55059159f0abc957
Jayascript <39142547+jayascript@users.noreply.github.com>
2019-11-17 05:44:24 -0500

    ch9

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -165,7 +165,14 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = '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'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
 
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
